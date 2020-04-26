745077e16bf3971c23d792945505ef4064c9c3fa
Pritam Nikam <pritam.nikam@gmail.com>
2020-04-26 15:51:24 +0530

    Optimized the implementation to avoid redundant interaction.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
+
 const Blockchain = require('./blockchain');
-const PubSub = require('./pubsub')
+const PubSub = require('./pubsub');
+
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 const app = express();
 const blockchain = new Blockchain();
@@ -15,23 +21,36 @@ app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
 });
 
-
 app.post('/api/mine', (req, res) => {
     const body = req.body;
     blockchain.addBlock(body);
-
     pubsub.broadcastChain();
-
     res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+    const requestBlockURL = `${ROOT_NODE_ADDRESS}/api/blocks`;
+    console.log(requestBlockURL);
+    request({ url: requestBlockURL}, (error , response, body) => {
+        if(!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log(`replace chain on the sync with: ${body}`);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+}
+
 let peer_port;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
     peer_port = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
+
 const PORT = peer_port || DEFAULT_PORT;
-app.listen(PORT, console.log(`listening on localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`listening on localhost:${PORT}`);
 
-
+    if (PORT !== DEFAULT_PORT) {
+        syncChains();
+    }
+});
