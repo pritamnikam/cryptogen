0a2097e7ef172dd8b3c67738aa4f157b54d31afd
Pritam Nikam <pritam.nikam@gmail.com>
2020-04-26 18:14:50 +0530

    Adds wallet and tests.

---
+++ wallet/index.test.js
@@ -0,0 +1,40 @@
+const Wallet = require('./index');
+const { verifySignature } = require('../util');
+
+
+describe('Wallet', () => {
+    let wallet;
+    beforeEach(()=> {
+        wallet = new Wallet();
+    });
+
+
+    it('has a `balance`', () => {
+        expect(wallet).toHaveProperty('balance');
+    });
+
+    it('has a `publicKey`', () => {
+        console.log(wallet.publicKey);
+        expect(wallet).toHaveProperty('publicKey');
+    });
+
+    describe('Signing data', () => {
+        const data = 'foobar';
+
+        it('verify a signature', () => {
+            expect(verifySignature({
+                publicKey: wallet.publicKey,
+                data,
+                signature: wallet.sign(data)
+            })).toBe(true);
+        });
+
+        it ('Does not verify an invalid signature.', () => {
+            expect(verifySignature({
+                publicKey: wallet.publicKey,
+                data,
+                signature: new Wallet().sign(data)
+            })).toBe(false);
+        });
+    });
+});
